84c13a42864e2be5b25a181a28e163275470a922
Sagar Kumar <aec.cse.sagarkumar@gmail.com>
2023-08-25 18:55:04 +0530

    Add files via upload

---
+++ DatabaseConnection.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+public class DatabaseConnection {
+    private static Connection connection = null;
+    private DatabaseConnection() {
+        // Empty private constructor
+    }
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName("org.sqlite.JDBC");
+                connection = DriverManager.getConnection("jdbc:sqlite:accounts.db");
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
